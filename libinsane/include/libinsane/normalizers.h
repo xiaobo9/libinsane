38b60bf1a24c78bfe599fa0795b4398dbc85bc5a
Jerome Flesch <jflesch@openpaper.work>
2018-07-06 09:00:57 +0200

    normalizers/source names: With some Sane drivers, flatbeds are called "Document Table"
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- libinsane/include/libinsane/normalizers.h
+++ libinsane/include/libinsane/normalizers.h
@@ -280,6 +280,7 @@ extern enum lis_error lis_api_normalizer_end_of_feed(
  * - All source names will be lower-case
  * - No WIA prefix
  * - All ADF will be called "feeder"
+ * - All flatbeds will be called "flatbed"
  *
  * Exanples when using WIA:
  * - '0000\\Root\\Flatbed' --> 'flatbed'
@@ -288,6 +289,8 @@ extern enum lis_error lis_api_normalizer_end_of_feed(
  * Examples when using Sane:
  * - 'Automatic document Feeder (left aligned)' --> 'feeder (left aligned)'
  * - 'ADF Duplex' --> 'feeder duplex'
+ * - 'Flatbed' --> 'flatbed'
+ * - 'Document Table' --> 'flatbed' (Epson perfection v19)
  *
  * \param[in] to_wrap Base implementation to wrap.
  * \param[out] impl Implementation of the API including the workaround.
