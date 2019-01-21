48963cfb0542ab51a17deca4c405290584c32d38
Jerome Flesch <jflesch@openpaper.work>
2019-01-21 21:34:52 +0100

    Libinsane: safe_defaults: Also set some default values again right before scanning (unless the application has set them itself). It is possibly for the scan area options: If they are expressed in pixels, when the app changes the resolution, it will change their constraint --> we need to re-maximize the scan area right before scanning.
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- subprojects/libinsane/src/normalizers/safe_defaults.c
+++ subprojects/libinsane/src/normalizers/safe_defaults.c
@@ -13,15 +13,29 @@
 
 #define NAME "normalizers_safe_defaults"
 
-// mode = Color
-// max scan area
-// set_opt(source, 'test-picture', 'Color pattern')
+/*!
+ * \brief set the default value immediately.
+ * Set the default value as soon as the item is requested.
+ * Note that it should be done for all values anyway, so the application
+ * can see the actual default values when requesting options.
+ */
+#define SET_IMMEDIATELY (1 << 0)
+
+/*!
+ * \brief set the default value right before scanning.
+ * Set the default value when the application request a scan session.
+ * This is recommended for options having constraints that may have changed
+ * when other options were changed (for instance, depending on the units,
+ * changing the resolution may change the scan area constraints).
+ */
+#define SET_BEFORE_SCAN (1 << 1)
 
 
 struct safe_setter {
 	const char *opt_name;
 	enum lis_error (*cb)(struct lis_option_descriptor *opt, void *cb_data, int *set_flags);
 	void *cb_data;
+	int flags;
 };
 
 
@@ -31,39 +45,104 @@ static enum lis_error set_preview(struct lis_option_descriptor *opt, void *cb_da
 static enum lis_error set_int(struct lis_option_descriptor *opt, void *cb_data, int *set_flags);
 static enum lis_error set_boolean(struct lis_option_descriptor *opt, void *cb_data, int *set_flags);
 
-static int g_numbers[] = { -1, 1, 0, 300, 24};
+static int g_numbers[] = { -1, 1, 0, 300, 24 };
 
 static const struct safe_setter g_safe_setters[] = {
 	// all backends:
-	{ .opt_name = OPT_NAME_MODE, .cb = set_str, .cb_data = OPT_VALUE_MODE_COLOR },
-	{ .opt_name = OPT_NAME_PREVIEW, .cb = set_preview, .cb_data = &g_numbers[2] /* false */ },
-	{ .opt_name = OPT_NAME_RESOLUTION, .cb = set_int, .cb_data = &g_numbers[3] /* 300 */ },
-	{ .opt_name = OPT_NAME_TL_X, .cb = set_to_limit, .cb_data = &g_numbers[0] },
-	{ .opt_name = OPT_NAME_TL_Y, .cb = set_to_limit, .cb_data = &g_numbers[0] },
-	{ .opt_name = OPT_NAME_BR_X, .cb = set_to_limit, .cb_data = &g_numbers[1] },
-	{ .opt_name = OPT_NAME_BR_Y, .cb = set_to_limit, .cb_data = &g_numbers[1] },
+	{
+		.opt_name = OPT_NAME_MODE, .cb = set_str,
+		.cb_data = OPT_VALUE_MODE_COLOR, .flags = SET_IMMEDIATELY,
+	},
+	{
+		.opt_name = OPT_NAME_PREVIEW, .cb = set_preview,
+		.cb_data = &g_numbers[2] /* false */, .flags = SET_IMMEDIATELY,
+	},
+	{
+		.opt_name = OPT_NAME_RESOLUTION, .cb = set_int,
+		.cb_data = &g_numbers[3] /* 300 */, .flags = SET_IMMEDIATELY,
+	},
+	{
+		.opt_name = OPT_NAME_TL_X, .cb = set_to_limit,
+		.cb_data = &g_numbers[0],
+		.flags = SET_IMMEDIATELY | SET_BEFORE_SCAN,
+	},
+	{
+		.opt_name = OPT_NAME_TL_Y, .cb = set_to_limit,
+		.cb_data = &g_numbers[0],
+		.flags = SET_IMMEDIATELY | SET_BEFORE_SCAN,
+	},
+	{
+		.opt_name = OPT_NAME_BR_X, .cb = set_to_limit,
+		.cb_data = &g_numbers[1],
+		.flags = SET_IMMEDIATELY | SET_BEFORE_SCAN,
+	},
+	{
+		.opt_name = OPT_NAME_BR_Y, .cb = set_to_limit,
+		.cb_data = &g_numbers[1],
+		.flags = SET_IMMEDIATELY | SET_BEFORE_SCAN,
+	},
 
 	// WORKAROUND(Jflesch): Fujistu SnapScan S1500 (Sane) + Fujistu SnapScan iX500 (Sane)
 	// page-width: Specifies the width of the media. Required for automatic centering of sheet-fed scans.
 	// page-height: Specifies the height of the media.
 	// ==> Default values are crap.
 	// ==> Since this feature is Fujistu-specific, here we disable automatic centering.
-	{ .opt_name = "page-width", .cb = set_to_limit, .cb_data = &g_numbers[1] },
-	{ .opt_name = "page-height", .cb = set_to_limit, .cb_data = &g_numbers[1] },
+	{
+		.opt_name = "page-width", .cb = set_to_limit,
+		.cb_data = &g_numbers[1],
+		.flags = SET_IMMEDIATELY | SET_BEFORE_SCAN,
+	},
+	{
+		.opt_name = "page-height", .cb = set_to_limit,
+		.cb_data = &g_numbers[1],
+		.flags = SET_IMMEDIATELY | SET_BEFORE_SCAN,
+	},
 
 	// Sane test backend:
-	{ .opt_name = "test-picture", .cb = set_str, .cb_data = "Color pattern" },
+	{
+		.opt_name = "test-picture", .cb = set_str,
+		.cb_data = "Color pattern",
+		.flags = SET_IMMEDIATELY,
+	},
 
 	// WIA2:
-	{ . opt_name = "pages", .cb = set_int, .cb_data = &g_numbers[2] /* 0 = infinite */ },
+	{
+		. opt_name = "pages", .cb = set_int,
+		.cb_data = &g_numbers[2], /* 0 = infinite */
+		.flags = SET_IMMEDIATELY,
+	},
 
 	// TWAIN:
-	{ .opt_name = "transfer_count", .cb = set_int, .cb_data = &g_numbers[0] /* -1 */ },
-	{ .opt_name = "compression", .cb = set_str, .cb_data = "none" },
-	{ .opt_name = "transfer_mechanism", .cb = set_str, .cb_data = "native" },
-	{ .opt_name = "image_file_format", .cb = set_str, .cb_data = "bmp" },
-	{ .opt_name = "bit_depth", .cb = set_int, .cb_data = &g_numbers[4] /* 24 */ },
-	{ .opt_name = "indicators", .cb = set_boolean, .cb_data = NULL /* FALSE */ },
+	{
+		.opt_name = "transfer_count", .cb = set_int,
+		.cb_data = &g_numbers[0], /* -1 */
+		.flags = SET_IMMEDIATELY,
+	},
+	{
+		.opt_name = "compression", .cb = set_str,
+		.cb_data = "none",
+		.flags = SET_IMMEDIATELY,
+	},
+	{
+		.opt_name = "transfer_mechanism", .cb = set_str,
+		.cb_data = "native",
+		.flags = SET_IMMEDIATELY,
+	},
+	{
+		.opt_name = "image_file_format", .cb = set_str,
+		.cb_data = "bmp",
+		.flags = SET_IMMEDIATELY,
+	},
+	{
+		.opt_name = "bit_depth", .cb = set_int,
+		.cb_data = &g_numbers[4], /* 24 */
+		.flags = SET_IMMEDIATELY,
+	},
+	{
+		.opt_name = "indicators", .cb = set_boolean,
+		.cb_data = NULL, /* FALSE */
+		.flags = SET_IMMEDIATELY,
+	},
 	{ .opt_name = NULL },
 };
 
@@ -251,56 +330,153 @@ static enum lis_error set_int(struct lis_option_descriptor *opt, void *cb_data, 
 }
 
 
+static enum lis_error set_default_values(
+		struct lis_item *item, int required_flag
+	)
+{
+	struct lis_option_descriptor **opts = NULL;
+	struct lis_option_descriptor **opt;
+	struct lis_option_descriptor *original;
+	const struct safe_setter *setter;
+	int set_flags;
+	enum lis_error err;
+
+	set_flags = LIS_SET_FLAG_MUST_RELOAD_OPTIONS;
+
+	for (setter = g_safe_setters; setter->opt_name != NULL ; setter++) {
+		if (!(setter->flags & required_flag)) {
+			continue;
+		}
+		lis_log_info(
+			NAME ": set_default_value(%s) ...",
+			setter->opt_name
+		);
+
+		if (set_flags & LIS_SET_FLAG_MUST_RELOAD_OPTIONS) {
+			lis_log_debug(
+				NAME ": set_default_value(%s): Must first:"
+				" Reload option list of item '%s'...",
+				setter->opt_name, item->name
+			);
+			err = item->get_options(item, &opts);
+			if (LIS_IS_ERROR(err)) {
+				lis_log_error(
+					NAME ": set_default_value(%s):"
+					" Failed to get "
+					" options of item '%s': 0x%X, %s",
+					setter->opt_name, item->name,
+					err, lis_strerror(err)
+				);
+				return err;
+			}
+			set_flags = 0;
+		}
+
+		for (opt = opts ; (*opt) != NULL ; opt++) {
+			if (strcasecmp(setter->opt_name, (*opt)->name) == 0) {
+				break;
+			}
+		}
+		if ((*opt) == NULL) {
+			lis_log_info(
+				NAME ": set_default_value(%s): Option not"
+				" found", setter->opt_name
+			);
+			continue;
+		}
+
+		if (lis_bw_opt_get_user_ptr(*opt) != NULL) {
+			lis_log_info(
+				NAME ": set_default_value(%s): Option already"
+				" set by user app. Won't set it to default"
+				" value.",
+				setter->opt_name
+			);
+			continue;
+		}
+
+		original = lis_bw_get_original_opt(*opt);
+		set_flags = 0;
+		err = setter->cb(original, setter->cb_data, &set_flags);
+		if (LIS_IS_OK(err)) {
+			lis_log_info(
+				NAME ": set_default_value(%s): OK",
+				setter->opt_name
+			);
+		} else {
+			lis_log_warning(
+				NAME ":set default_value(%s):"
+				" Failed to set option"
+				" to safe default: 0x%X, %s",
+				(*opt)->name,
+				err, lis_strerror(err)
+			);
+			// still worth trying scanning
+		}
+	}
+
+	return LIS_OK;
+}
+
+
+static enum lis_error scan_start(struct lis_item *self, struct lis_scan_session **session)
+{
+	enum lis_error err;
+	struct lis_item *original = lis_bw_get_original_item(self);
+
+	lis_log_info("Scan start requested. Setting some late default values");
+	err = set_default_values(self, SET_BEFORE_SCAN);
+	if (LIS_IS_ERROR(err)) {
+		return err;
+	}
+	return original->scan_start(original, session);
+}
+
+
 static enum lis_error item_filter(struct lis_item *item, int root, void *user_data)
 {
 	enum lis_error err;
-	struct lis_option_descriptor **opts;
-	int opt_idx;
-	const struct safe_setter *setter;
-	int set_flags;
-	int setter_idx;
 
 	LIS_UNUSED(user_data);
+	LIS_UNUSED(root);
 
-	for (setter_idx = 0; g_safe_setters[setter_idx].opt_name != NULL ; ) {
-
-		lis_log_debug(NAME "->item_filter(): Getting options for item '%s' (%d)...", item->name, root);
-		err = item->get_options(item, &opts);
-		if (LIS_IS_ERROR(err)) {
-			lis_log_error(NAME "->item_filter(): Failed to get options of item '%s' (%d): 0x%X, %s",
-				item->name, root, err, lis_strerror(err));
-			return err;
-		}
-
-		for (; g_safe_setters[setter_idx].opt_name != NULL ; setter_idx++) {
-			set_flags = 0;
-			setter = &g_safe_setters[setter_idx];
-
-			for (opt_idx = 0 ; opts[opt_idx] != NULL ; opt_idx++) {
-				if (strcasecmp(setter->opt_name, opts[opt_idx]->name) != 0) {
-					continue;
-				}
-
-				err = setter->cb(opts[opt_idx], setter->cb_data, &set_flags);
-				if (LIS_IS_ERROR(err)) {
-					lis_log_warning(
-						NAME "->item_filter(): Failed to set option '%s'"
-						" to safe default: 0x%X, %s",
-						opts[opt_idx]->name, err, lis_strerror(err)
-					);
-					// still worth trying scanning
-				}
-				break;
-			}
-			if (set_flags & LIS_SET_FLAG_MUST_RELOAD_OPTIONS) {
-				lis_log_info("Must reload options");
-				setter_idx++;
-				break;
-			}
-		}
-
+	item->scan_start = scan_start;
+	lis_log_info("Setting default values on item '%s'", item->name);
+	err = set_default_values(item, SET_IMMEDIATELY);
+	if (LIS_IS_ERROR(err)) {
+		return err;
 	}
+	return LIS_OK;
+}
 
+
+static enum lis_error opt_set_value(
+		struct lis_option_descriptor *self,
+		union lis_value value, int *set_flags
+	)
+{
+	enum lis_error err;
+	struct lis_option_descriptor *original = lis_bw_get_original_opt(self);
+
+	err = original->fn.set_value(original, value, set_flags);
+
+	if (LIS_IS_OK(err)) {
+		// put a dummy non-NULL user pointer to mark the option as set
+		lis_bw_opt_set_user_ptr(self, (void*)0xDEADBEEF, NULL);
+	}
+	return err;
+}
+
+
+static enum lis_error opt_desc_filter(
+		struct lis_item *item, struct lis_option_descriptor *desc,
+		void *user_data
+	)
+{
+	LIS_UNUSED(item);
+	LIS_UNUSED(user_data);
+
+	desc->fn.set_value = opt_set_value;
 	return LIS_OK;
 }
 
@@ -311,6 +487,7 @@ enum lis_error lis_api_normalizer_safe_defaults(struct lis_api *to_wrap, struct 
 	err = lis_api_base_wrapper(to_wrap, impl, NAME);
 	if (LIS_IS_OK(err)) {
 		lis_bw_set_item_filter(*impl, item_filter, NULL);
+		lis_bw_set_opt_desc_filter(*impl, opt_desc_filter, NULL);
 	}
 	return err;
 }
--- subprojects/libinsane/tests/tests_normalizer_safe_defaults.c
+++ subprojects/libinsane/tests/tests_normalizer_safe_defaults.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
 
 #include <CUnit/Basic.h>
@@ -95,6 +96,28 @@ static int tests_opt_init(void)
 	static const union lis_value opt_resolution_default = {
 		.integer = 50,
 	};
+	// we won't actually scan anything, but we want to start a scan session
+	// so we need to feed the dumb implementation parameters & content.
+	static const struct lis_scan_parameters base_scan_params = {
+		.format = LIS_IMG_FORMAT_BMP,
+		.width = 2222,
+		.height = 2222,
+		.image_size = 22222222,
+	};
+	static const uint8_t header_a[] = {
+		0x42, 0x4d,
+	};
+	static const uint8_t header_b[] = {
+		0x00, 0x00,
+	};
+	static const uint8_t body[] = {
+		0xFF, 0xFF, 0xFF,
+	};
+	static const struct lis_dumb_read reads[] = {
+		{ .content = header_a, .nb_bytes = LIS_COUNT_OF(header_a) },
+		{ .content = header_b, .nb_bytes = LIS_COUNT_OF(header_b) },
+		{ .content = body, .nb_bytes = LIS_COUNT_OF(body) },
+	};
 	enum lis_error err;
 
 	g_opt = NULL;
@@ -116,6 +139,8 @@ static int tests_opt_init(void)
 		g_dumb, &opt_resolution, &opt_resolution_default,
 		LIS_SET_FLAG_MUST_RELOAD_PARAMS
 	);
+	lis_dumb_set_scan_parameters(g_dumb, &base_scan_params);
+	lis_dumb_set_scan_result(g_dumb, reads, LIS_COUNT_OF(reads));
 
 	return 0;
 }
@@ -127,6 +152,7 @@ static int tests_opt_clean(void)
 	api->cleanup(api);
 	return 0;
 }
+
 
 static void tests_opt_defaults(void)
 {
@@ -166,6 +192,127 @@ static void tests_opt_defaults(void)
 }
 
 
+static void tests_opt_defaults_before_scan(void)
+{
+	enum lis_error err;
+	struct lis_item *defaults_item = NULL;
+	struct lis_item *dumb_item = NULL;
+	struct lis_option_descriptor **defaults_opts = NULL;
+	struct lis_option_descriptor **dumb_opts = NULL;
+	struct lis_scan_session *session;
+	union lis_value value;
+	int set_flags;
+
+	LIS_ASSERT_EQUAL(tests_opt_init(), 0);
+
+	err = lis_api_normalizer_safe_defaults(g_dumb, &g_opt);
+	LIS_ASSERT_EQUAL(err, LIS_OK);
+
+	err = g_opt->get_device(
+		g_opt, LIS_DUMB_DEV_ID_FIRST, &defaults_item
+	);
+	LIS_ASSERT_EQUAL(err, LIS_OK);
+
+	err = g_dumb->get_device(g_dumb, LIS_DUMB_DEV_ID_FIRST, &dumb_item);
+	LIS_ASSERT_EQUAL(err, LIS_OK);
+
+	err = defaults_item->get_options(defaults_item, &defaults_opts);
+	LIS_ASSERT_EQUAL(err, LIS_OK);
+	LIS_ASSERT_EQUAL(strcasecmp(defaults_opts[0]->name, OPT_NAME_MODE), 0);
+	LIS_ASSERT_EQUAL(strcasecmp(defaults_opts[1]->name, OPT_NAME_TL_X), 0);
+	LIS_ASSERT_EQUAL(strcasecmp(defaults_opts[2]->name, OPT_NAME_RESOLUTION), 0);
+	LIS_ASSERT_EQUAL(defaults_opts[3], NULL);
+
+	// change the value without going through the safe_defaults
+	// normalizer --> it should reset the value right before scanning
+	err = dumb_item->get_options(dumb_item, &dumb_opts);
+	LIS_ASSERT_EQUAL(err, LIS_OK);
+	value.integer = 200;
+	err = dumb_opts[1]->fn.set_value(dumb_opts[1], value, &set_flags);
+	LIS_ASSERT_EQUAL(err, LIS_OK);
+
+	err = defaults_opts[1]->fn.get_value(defaults_opts[1], &value);
+	LIS_ASSERT_EQUAL(err, LIS_OK);
+	LIS_ASSERT_EQUAL(value.integer, 200);
+
+	session = NULL;
+	err = defaults_item->scan_start(defaults_item, &session);
+	LIS_ASSERT_TRUE(LIS_IS_OK(err));
+	LIS_ASSERT_NOT_EQUAL(session, NULL);
+
+	err = dumb_opts[1]->fn.get_value(dumb_opts[1], &value);
+	LIS_ASSERT_EQUAL(err, LIS_OK);
+	LIS_ASSERT_EQUAL(value.integer, 100); // changed back
+
+	session->cancel(session);
+
+	defaults_item->close(defaults_item);
+	LIS_ASSERT_EQUAL(tests_opt_clean(), 0);
+}
+
+
+static void tests_opt_defaults_before_scan2(void)
+{
+	enum lis_error err;
+	struct lis_item *defaults_item = NULL;
+	struct lis_item *dumb_item = NULL;
+	struct lis_option_descriptor **defaults_opts = NULL;
+	struct lis_option_descriptor **dumb_opts = NULL;
+	struct lis_scan_session *session;
+	union lis_value value;
+	int set_flags;
+
+	LIS_ASSERT_EQUAL(tests_opt_init(), 0);
+
+	err = lis_api_normalizer_safe_defaults(g_dumb, &g_opt);
+	LIS_ASSERT_EQUAL(err, LIS_OK);
+
+	err = g_opt->get_device(
+		g_opt, LIS_DUMB_DEV_ID_FIRST, &defaults_item
+	);
+	LIS_ASSERT_EQUAL(err, LIS_OK);
+
+	err = g_dumb->get_device(g_dumb, LIS_DUMB_DEV_ID_FIRST, &dumb_item);
+	LIS_ASSERT_EQUAL(err, LIS_OK);
+
+	err = defaults_item->get_options(defaults_item, &defaults_opts);
+	LIS_ASSERT_EQUAL(err, LIS_OK);
+	LIS_ASSERT_EQUAL(strcasecmp(defaults_opts[0]->name, OPT_NAME_MODE), 0);
+	LIS_ASSERT_EQUAL(strcasecmp(defaults_opts[1]->name, OPT_NAME_TL_X), 0);
+	LIS_ASSERT_EQUAL(strcasecmp(defaults_opts[2]->name, OPT_NAME_RESOLUTION), 0);
+	LIS_ASSERT_EQUAL(defaults_opts[3], NULL);
+
+	err = dumb_item->get_options(dumb_item, &dumb_opts);
+	LIS_ASSERT_EQUAL(err, LIS_OK);
+
+	// change the value going through the safe_detaults
+	// normalizer (as the application would do)
+	// --> it shouldn't reset the value
+	value.integer = 200;
+	err = defaults_opts[1]->fn.set_value(
+		defaults_opts[1], value, &set_flags
+	);
+	LIS_ASSERT_EQUAL(err, LIS_OK);
+
+	err = defaults_opts[1]->fn.get_value(defaults_opts[1], &value);
+	LIS_ASSERT_EQUAL(err, LIS_OK);
+	LIS_ASSERT_EQUAL(value.integer, 200);
+
+	session = NULL;
+	err = defaults_item->scan_start(defaults_item, &session);
+	LIS_ASSERT_TRUE(LIS_IS_OK(err));
+	LIS_ASSERT_NOT_EQUAL(session, NULL);
+
+	err = dumb_opts[1]->fn.get_value(dumb_opts[1], &value);
+	LIS_ASSERT_EQUAL(err, LIS_OK);
+	LIS_ASSERT_EQUAL(value.integer, 200); // unchanged
+
+	session->cancel(session);
+
+	defaults_item->close(defaults_item);
+	LIS_ASSERT_EQUAL(tests_opt_clean(), 0);
+}
+
 int register_tests(void)
 {
 	CU_pSuite suite = NULL;
@@ -176,7 +323,11 @@ int register_tests(void)
 		return 0;
 	}
 
-	if (CU_add_test(suite, "tests_opt_defaults()", tests_opt_defaults) == NULL) {
+	if (CU_add_test(suite, "opt_defaults", tests_opt_defaults) == NULL
+			|| CU_add_test(suite, "opt_defaults_before_scan()",
+				tests_opt_defaults_before_scan) == NULL
+			|| CU_add_test(suite, "opt_defaults_before_scan2()",
+				tests_opt_defaults_before_scan2) == NULL) {
 		fprintf(stderr, "CU_add_test() has failed\n");
 		return 0;
 	}
