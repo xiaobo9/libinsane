cba1221cbb1a885a87f71c2e7ef53fcda9f38e60
Jerome Flesch <jflesch@openpaper.work>
2021-06-13 19:06:25 +0200

    deliver_doc.sh: Moved to another object storage

--- deliver_doc.sh
+++ deliver_doc.sh
@@ -2,7 +2,7 @@
 
 directory=doc/build
 
-if [ -z "$RCLONE_CONFIG_OVHSWIFT_USER" ] ; then
+if [ -z "$RCLONE_CONFIG_OPENPAPERWORK_ACCESS_KEY_ID" ] ; then
   echo "Delivery: No rclone credentials provided."
   exit 0
 fi
@@ -17,12 +17,12 @@ echo "Delivering: ${directory} (${CI_COMMIT_REF_NAME} - ${CI_COMMIT_SHORT_SHA})"
 out_name="$(date "+%Y%m%d_%H%M%S")_${CI_COMMIT_REF_NAME}_${CI_COMMIT_SHORT_SHA}"
 latest_name="latest"
 
-if ! rclone --config ./rclone.conf copy "${directory}/" "ovhswift:documentation/libinsane/${out_name}" ; then
+if ! rclone --config ./rclone.conf copy "${directory}/" "openpaperwork:documentation/libinsane/${out_name}" ; then
   echo "rclone failed"
   exit 1
 fi
 
-if ! rclone --config ./rclone.conf sync "${directory}/" "ovhswift:documentation/libinsane/latest" ; then
+if ! rclone --config ./rclone.conf sync "${directory}/" "openpaperwork:documentation/libinsane/latest" ; then
   echo "rclone failed"
   exit 1
 fi
