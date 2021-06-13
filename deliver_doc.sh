96321f8b8b352c38a4ab47856adc8bff6d950216
Jerome Flesch <jflesch@openpaper.work>
2021-06-13 23:05:04 +0200

    CI: deliver-doc: Fix bucket name
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- deliver_doc.sh
+++ deliver_doc.sh
@@ -17,12 +17,12 @@ echo "Delivering: ${directory} (${CI_COMMIT_REF_NAME} - ${CI_COMMIT_SHORT_SHA})"
 out_name="$(date "+%Y%m%d_%H%M%S")_${CI_COMMIT_REF_NAME}_${CI_COMMIT_SHORT_SHA}"
 latest_name="latest"
 
-if ! rclone --config ./rclone.conf copy "${directory}/" "openpaperwork:documentation/libinsane/${out_name}" ; then
+if ! rclone --config ./rclone.conf copy "${directory}/" "openpaperwork:openpaperwork-doc/libinsane/${out_name}" ; then
   echo "rclone failed"
   exit 1
 fi
 
-if ! rclone --config ./rclone.conf sync "${directory}/" "openpaperwork:documentation/libinsane/latest" ; then
+if ! rclone --config ./rclone.conf sync "${directory}/" "openpaperwork:openpaperwork-doc/libinsane/latest" ; then
   echo "rclone failed"
   exit 1
 fi
